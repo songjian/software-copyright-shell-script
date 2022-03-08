b51aa5ad76637112f6a534460698da2d8ee9b4ee
sj <songjian@codeorder.cn>
2022-03-08 19:31:50 +0800

    更新脚本

--- newsapp-ios.sh
+++ newsapp-ios.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-outfile=newsapp-ios2-code.txt
+outfile=newsapp-ios-code.txt
 outdir=outfile
 find ../newsapp/APP/ -type f '(' -name '*.html' -o -name '*.css' -o -name '*.js' ')' \
-    -exec cat {} \; > $outdir/$outfile
-dos2unix $outdir/$outfile
-sed -i 's#//.*##g' $outdir/$outfile
-sed -i 's/速翔网络/资讯站/g' $outdir/$outfile
-sed -i 's/QQ技术：//g' $outdir/$outfile
-sed -i 's/1424445608//g' $outdir/$outfile
-sed -i "s/<!--.*-->//g" $outdir/$outfile
-sed -i 's/^\s*$//g' $outdir/$outfile
-sed -i '/^$/d' $outdir/$outfile
+    -exec cat {} \; | dos2unix | sed -f script > $outdir/$outfile
