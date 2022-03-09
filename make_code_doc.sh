6b29ef7ed923943065eea277982e158180024694
sj <songjian@codeorder.cn>
2022-03-09 17:58:03 +0800

    更新程序

--- make_code_doc.sh
+++ make_code_doc.sh
@@ -3,3 +3,6 @@ code_dir=$1
 out_file=$2
 find ${code_dir} -type f '(' -name '*.html' -o -name '*.css' -o -name '*.js' -o -name '*.php' ')' \
     -exec cat {} \; | dos2unix | sed -f script > ${out_file}
+
+source venv/bin/activate
+python make_docx.py ${out_file}
---
+++ make_docx.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from docx import Document
+from docx.shared import Pt
+from docx.oxml.ns import qn
+from docx.shared import Cm
+import os
+import sys
+
+f=open(sys.argv[1],mode='r',encoding='utf-8')
+code=f.read()
+
+path = sys.argv[1][0:-4] + ".docx"
+os.system("touch %s" %path)
+document = Document('examples.docx')
+document.styles['Normal'].font.name = u'Times New Roman' #设置西文字体
+document.styles['Normal']._element.rPr.rFonts.set(qn('w:eastAsia'), u'宋体') #设置中文字体使用字体2->宋体
+document.sections[0].top_margin = Cm(2.04)
+document.sections[0].bottom_margin = Cm(2.04)
+document.sections[0].left_margin = Cm(2.54)
+document.sections[0].right_margin = Cm(2.54)
+p = document.add_paragraph(code)
+p.style.font.size=Pt(10.5)
+p.paragraph_format.line_spacing=Pt(14)
+document.save(path) 
